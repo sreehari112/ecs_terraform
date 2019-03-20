e80a300ced41a694988b719228d0b1fad6fe2fc9
sreehari112 <38272677+sreehari112@users.noreply.github.com>
2019-03-20 19:19:55 +0530

    Update application-load-balancer.tf

--- application-load-balancer.tf
+++ application-load-balancer.tf
@@ -41,3 +41,15 @@ resource "aws_alb_listener" "alb-listener" {
         type             = "forward"
     }
 }
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.ecs-target-group}"
+  }
+}
